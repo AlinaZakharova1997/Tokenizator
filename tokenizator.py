dd8635a50335c838e59d7a214e830a417503391c
AlinaZakharova1997 <lina.zakharova2012@yandex.ru>
2018-10-05 13:35:53 +0300

    changed commentary

--- tokenizator.py
+++ tokenizator.py
@@ -1,6 +1,9 @@
 """
 Tokenizator
-This module performs the morphological analyses of a text and extracts tokens
+This module performs the morphological analyses of a text and extracts tokens.
+Tokens here are words or letters
+As a result it returns a list of tokens
+and a position of the first element of each token.
 """
 class Token(object):
     """
