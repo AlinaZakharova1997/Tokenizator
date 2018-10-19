47753456a38b1d57a4bdce8818e3b0bdfceb6e32
AlinaZakharova1997 <43806590+AlinaZakharova1997@users.noreply.github.com>
2018-10-19 13:52:16 +0300

    Update tokenizator.py

--- tokenizator.py
+++ tokenizator.py
@@ -83,7 +83,71 @@ class Tokenizator(object):
             t=Token(position, s)
             yield(t)
     
-  
+    @staticmethod
+    def tokens_type_definition(x):
+       """
+       This is a static method, which defines a type of a token
+       @return: type of a token
+       """
+       if x.isalpha():
+           tp = 'alpha'
+       if x.isdigit():
+           tp = 'digit'
+       if x.isspace():
+           tp = 'space'
+       if unicodedata.category(x)[0] == 'P':
+           tp = 'punct'
+       return tp      
+      
+    def tokens_generator_plus_type (self, strim):
+        """
+        This is a generator.
+        @param strim:strim is a text given
+        @return: tokens from the given text plus their type
+        """
+        if  not isinstance(strim, str):
+            raise ValueError('Input has an unappropriate type, it should be str')
+        position=0
+        for i,c in enumerate(strim):  # i is a number c is a letter
+            # here it's just checks if types are equal
+            if self.tokens_type_definition(c) != self.tokens_type_definition(strim[i-1]) and i>0:
+                tp = self.tokens_type_definition(strim[i-1])
+                s = strim[position:i]
+                position = i
+                t = Token_Type(s,tp)                        
+                yield(t)
+        # last if for the very last substring in strim        
+        if self.tokens_type_definition(c):
+            tp = self.tokens_type_definition(c)
+            s = strim[position:i+1]
+            t = Token_Type(s,tp)
+            yield(t)
+            
+    def tokens_generator_plus_type_optimized (self, strim):
+        """
+        This is a generator.
+        @param strim:strim is a text given
+        @return: tokens from the given text plus their type
+        """
+        if  not isinstance(strim, str):
+            raise ValueError('Input has an unappropriate type, it should be str')
+        position=0
+        tp_of_c=self.tokens_type_definition(strim[0])
+        for i,c in enumerate(strim):  # i is a number c is a letter
+            # here it's just checks if types are equal
+            if self.tokens_type_definition(c) != tp_of_c and i>0:
+                tp = self.tokens_type_definition(strim[i-1])
+                tp_of_c = self.tokens_type_definition(c)
+                s = strim[position:i]
+                position = i
+                t = Token_Type(s,tp)                        
+                yield(t)    
+        # last if for the very last substring in strim        
+        if self.tokens_type_definition(c):
+            tp = self.tokens_type_definition(c)
+            s = strim[position:i+1]
+            t = Token_Type(s,tp)
+            yield(t)        
     
 x=Tokenizator()
 for i in x.tokens_generator(' Ф 12 !!! @ # Alina is a student)))'):
