9c7ac8a8c04426f5624894779e8c7440cddf070a
AlinaZakharova1997 <43806590+AlinaZakharova1997@users.noreply.github.com>
2018-11-16 17:51:29 +0300

    Update tokenizator.py

--- tokenizator.py
+++ tokenizator.py
@@ -1,10 +1,7 @@
 import unicodedata
 """
 Tokenizator
-This module performs the morphological analyses of a text and extracts tokens.
-Tokens here are words or letters
-As a result it returns a list of tokens
-and a position of the first element of each token.
+This module performs the morphological analyses of a text and extracts tokens
 """
 class Token(object):
     """
@@ -26,7 +23,32 @@ class Token(object):
         The way the programm returns the final result.
         """
         return self.s+'_'+str(self.position)
+    
 
+    
+class Token_Type(Token):
+    """
+    Class of tokens taken from a given text
+    """
+    def __init__(self,s,tp,position):
+        """
+        Consrtuctor for token.
+        @param self: self is an odject(here:token) with attributes
+        @param s: s is a string view of a token
+        @param tp: tp is a type of a token
+        @return: token with it's type
+        """
+        self.s=s
+        self.tp=tp
+        self.position=position
+        
+    def __repr__(self):
+        """
+        The way the programm returns the final result.
+        """
+        """return self.s+ '_' + self.tp"""
+        return self.s
+    
     
 class Tokenizator(object):
     """
@@ -40,7 +62,7 @@ class Tokenizator(object):
         """
         if  not isinstance(strim,str):
             raise ValueError('Input has an unappropriate type, it should be str')
-        tokensback=[]
+        tokensback = []
         for i,c in enumerate(strim):  # i is a number c is a letter
             # here it's just shifts to the position where token starts
             if c.isalpha() and (not strim[i-1].isalpha() or i==0):   
@@ -69,21 +91,21 @@ class Tokenizator(object):
             raise ValueError('Input has an unappropriate type, it should be str')
         for i,c in enumerate(strim):  # i is a number c is a letter
             # here it's just shifts to the position where token starts
-            if c.isalpha() and (not strim[i-1].isalpha() or i==0):   
+            if c.isalpha() and (not strim[i-1].isalpha() or i == 0):   
                 position = i
             # here it takes section from the beginning to the end of the token
             # and adds token to the list
             if not c.isalpha()and i>0 and strim[i-1].isalpha():   
                 s = strim[position:i]
                 # constructor for token is working here
-                t=  Token(position, s)                        
+                t = Token(position, s)                        
                 yield(t)
         # last if for the very last substring in strim        
         if c.isalpha():                   
-            s=strim[position:i+1]
-            t=Token(position, s)
+            s = strim[position:i+1]
+            t = Token(position, s)
             yield(t)
-    
+            
     @staticmethod
     def tokens_type_definition(x):
        """
@@ -136,23 +158,31 @@ class Tokenizator(object):
         tp_of_c=self.tokens_type_definition(strim[0])
         for i,c in enumerate(strim):  # i is a number c is a letter
             # here it's just checks if types are equal
-            if i>0 and self.tokens_type_definition(c) != tp_of_c :
-                tp =  tp_of_c
+            if i>0 and self.tokens_type_definition(c) != tp_of_c:
+                tp = tp_of_c
                 tp_of_c = self.tokens_type_definition(c)
                 s = strim[position:i]
                 position = i
-                t = Token_Type(s,tp)                        
-                yield(t)           
+                t = Token_Type(s,tp,position)                        
+                yield(t)    
+        # last if for the very last substring in strim        
+        '''if self.tokens_type_definition(c):'''
         tp = self.tokens_type_definition(c)
         s = strim[position:i+1]
-        t = Token_Type(s,tp)
-        yield(t)        
-    
+        t = Token_Type(s,tp,position)
+        yield(t)
+        
+    def token_gen (self, strim):
+        for token in self.tokens_generator_plus_type_optimized (strim):
+            if token.tp == 'alpha' or token.tp == 'digit':
+                yield(token)
+
+
+
+       
 if __name__ == '__main__':
     x=Tokenizator()
-    for i in x.tokens_generator_plus_type_optimized(' Ф 12 !!! @ # Alina is a student)))'):
+    for i in x.token_gen(' Ф 12 !!! @ # Alina is a student)))'):
         print(i)
-
-
 
 
