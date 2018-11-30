6a70604ec444c918b2d477b623f79b40451c704b
AlinaZakharova1997 <43806590+AlinaZakharova1997@users.noreply.github.com>
2018-11-30 16:35:12 +0300

    Update indexer.py

--- indexer.py
+++ indexer.py
@@ -16,12 +16,12 @@ class Position(object):
     @param end: position on the last element of a token
     """
 
-    def __init__(self,start,end):
+    def __init__(self, start, end):
         
         self.start = start
         self.end = end
         
-    def __eq__(self,position):
+    def __eq__(self, position):
 
         return self.start == position.start and self.end == position.end
 
@@ -39,17 +39,17 @@ class Position_Plus(Position):
     @param lnumber: number of a line in a given text
     """
 
-    def __init__(self,lnumber,start,end):
+    def __init__(self, lnumber, start, end):
         
         self.start = start
         self.end = end
         self.lnumber = lnumber
         
-    def __eq__(self,position):
+    def __eq__(self, position):
 
         return self.lnumber == position.lnumber and self.start == position.start and self.end == position.end 
 
-    def __lt__(self,other_pos):
+    def __lt__(self, other_pos):
         
         if self.lnumber < other_pos.lnumber or self.lnumber > other_pos.lnumber :
             return self.lnumber < other_pos.lnumber or self.lnumber > other_pos.lnumber 
@@ -62,7 +62,7 @@ class Position_Plus(Position):
           
 class Indexer(object):
 
-    def __init__(self,database):
+    def __init__(self, database):
         """
         Constructor for an database
         @param database: element of Indexer,
@@ -78,32 +78,32 @@ class Indexer(object):
         self.database.close()
         
 
-    def get_index(self,filename):
+    def get_index(self, filename):
         """
         This function performs indexing of a text in a given file
         """
-        if  not isinstance(filename,str):
+        if  not isinstance(filename, str):
             raise TypeError('Input has an unappropriate type!')
-        my_file=open(filename,encoding='utf-8')
+        my_file = open(filename,encoding='utf-8')
         for token in self.tokenizator.token_gen(my_file.read()):
             start = token.position
             end = start + len(token.s)
-            pos = Position(start,end)
-            self.database.setdefault(token.s,{}).setdefault(filename,[]).append(pos)
+            pos = Position(start, end)
+            self.database.setdefault(token.s, {}).setdefault(filename, []).append(pos)
         my_file.close()    
             
-    def get_index_with_line(self,filename):
+    def get_index_with_line(self, filename):
         """
         This function performs indexing of a text in a given file
         """
-        if  not isinstance(filename,str):
+        if  not isinstance(filename, str):
             raise TypeError('Input has an unappropriate type!')
-        my_file = open(filename,encoding='utf-8')
+        my_file = open(filename, encoding='utf-8')
         for lnumber,line in enumerate(my_file):
             for token in self.tokenizator.token_gen(line):
                 start = token.position
                 end = start + len(token.s)
-                pos = Position_Plus(lnumber,start,end)
-                self.database.setdefault(token.s,{}).setdefault(filename,[]).append(pos)
+                pos = Position_Plus(lnumber, start, end)
+                self.database.setdefault(token.s, {}).setdefault(filename, []).append(pos)
             lnumber+=1
         my_file.close() 
