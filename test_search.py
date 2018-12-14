ec1c990ee4001d7460887d8608794a1a4871e462
AlinaZakharova1997 <43806590+AlinaZakharova1997@users.noreply.github.com>
2018-12-14 14:01:30 +0300

    Update test_search.py

--- test_search.py
+++ test_search.py
@@ -6,28 +6,53 @@ import shelve
 import indexer
 from indexer import Indexer, Position_Plus
 
+
 class TestMyCode(unittest.TestCase):
     
     def setUp(self):
-        self.database = SearchEngine('database')
-        self.indexator = Indexer('database')
         self.maxDiff = None
 
+    def tearDown(self):
+        del self.search
+        # for current directory use '.'
+        file_list = os.listdir(path=".")
+        for i in file_list:
+            if i == 'database':
+                database_exists = True
+                os.remove(i)
+            elif i.startswith('database.'):
+                database_exists= True
+                os.remove(i)    
+
     def test_MyError_give_dict_inp(self):
+        self.search = SearchEngine('database')
         with self.assertRaises(TypeError):
-            self.database.get_dict(12)
+            self.search.get_dict(12)
             
     def test_get_dict(self):
-        test_file = open('test_search_one.txt', 'w',encoding='utf-8') 
+        self.indexator = Indexer('database')
+        test_file = open('test_search_one.txt', 'w') 
         test_file.write(' Ф 12 !!! @ # Alina is a student)))')
         test_file.close()
-        self.indexator.get_index('test_search_one.txt')
+        self.indexator.get_index_with_line('test_search_one.txt')
         del self.indexator
-        database = dict(shelve.open('database'))
-        self.database.get_dict('Alina')
+        self.search = SearchEngine('database')
+        result = self.search.get_dict('Alina')
         cool_result = {'test_search_one.txt': [Position_Plus(0,14,19)]}
-        self.assertEqual(database, cool_result)
+        self.assertEqual(result, cool_result)
         os.remove('test_search_one.txt')
         
+    def test_dict_no_such_token(self):
+        self.indexator = Indexer('database')
+        test_file = open('test_search_one.txt', 'w') 
+        test_file.write(' Ф 12 !!! @ # Alina is a student)))')
+        test_file.close()
+        self.indexator.get_index_with_line('test_search_one.txt')
+        del self.indexator
+        self.search = SearchEngine('database')
+        result = self.search.get_dict('Laptop')
+        os.remove('test_search_one.txt')
+
+        
 if __name__ == '__main__':
-    unittest.main()    
+    unittest.main()      
