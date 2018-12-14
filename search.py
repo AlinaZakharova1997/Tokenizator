606504a84e55cdfd34fd07e3ffe8d25ceb9354e3
AlinaZakharova1997 <43806590+AlinaZakharova1997@users.noreply.github.com>
2018-12-14 14:00:44 +0300

    Update search.py

--- search.py
+++ search.py
@@ -1,32 +1,39 @@
-'''
+"""
 SearchEngine
 This module does searching for positions of tokens in database
-'''
+"""
 import os
 import shelve
 import indexer
 from indexer import Indexer, Position_Plus
 
+
 class SearchEngine(object):
-    '''
+    """
     class SearchEngine
     @param database: datadase of tokens and thier positions
-    '''
+    """
     
     def __init__(self,database):
 
         self.database = shelve.open(database,writeback=True)
+
+    def __del__(self):
+        self.database.close()    
             
     def get_dict (self,tok_str):
-        '''
+        """
         This function performs searching for positions of a given token
         @param tok_str: str containing token
         @return: dictionary, where a key is a filename
         and a value is a list of positions
-        ''' 
+        """
         
         if  not isinstance(tok_str,str):
             raise TypeError('Input has an unappropriate type!')
 
         if tok_str in self.database:
             return self.database[tok_str]
+        else:
+            return {} 
+            
