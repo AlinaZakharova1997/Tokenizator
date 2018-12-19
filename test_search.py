26842334640fd99357abe7ff676b87baa2e3904f
AlinaZakharova1997 <43806590+AlinaZakharova1997@users.noreply.github.com>
2018-12-19 10:19:32 +0300

    Update test_search.py

--- test_search.py
+++ test_search.py
@@ -52,7 +52,55 @@ class TestMyCode(unittest.TestCase):
         self.search = SearchEngine('database')
         result = self.search.get_dict('Laptop')
         os.remove('test_search_one.txt')
-
         
+    def test_MyError_dive_dict_many_tokens_inp(self):
+       self.search = SearchEngine('database')
+        with self.assertRaises(TypeError):
+            self.search.get_dict_many_tokens(12)
+
+    def test_get_dict_many_tokens(self):
+       self.indexator = Indexer('database')
+        test_file_one = open('test_search_one.txt', 'w') 
+        test_file_one.write(' Ф 12 !!! @ # Alina is a student)))')
+        test_file_one.close()
+        test_file_two = open('test_search_two.txt', 'w') 
+        test_file_two.write(' Ф 12 !!! @ # Alina loves apples)))')
+        test_file_two.close()
+        test_file_three = open('test_search_three.txt', 'w') 
+        test_file_three.write(' Ф 12 !!! @ # Alina ££ student)))')
+        test_file_three.close()
+        self.indexator.get_index_with_line('test_search_one.txt')
+        self.indexator.get_index_with_line('test_search_two.txt')
+        self.indexator.get_index_with_line('test_search_three.txt')
+        del self.indexator
+        self.search = SearchEngine('database')
+        result = self.search.get_dict_many_tokens('Alina is a student')
+        cool_result = {'test_search_one.txt': [Position_Plus(0,14,19),Position_Plus(0,20,22),
+                                               Position_Plus(0,23,24),Position_Plus(0,25,32)]}
+        self.assertEqual(result, cool_result)
+        os.remove('test_search_one.txt')
+        os.remove('test_search_two.txt')
+        os.remove('test_search_three.txt')
+
+    def test_dict_no_tokens_found(self):
+        test_file_one = open('test_search_one.txt', 'w') 
+        test_file_one.write(' Ф 12 !!! @ # Alina is a student)))')
+        test_file_one.close()
+        test_file_two = open('test_search_two.txt', 'w') 
+        test_file_two.write(' Ф 12 !!! @ # Alina loves apples)))')
+        test_file_two.close()
+        test_file_three = open('test_search_three.txt', 'w') 
+        test_file_three.write(' Ф 12 !!! @ # Alina ££ student)))')
+        test_file_three.close()
+        self.indexator.get_index_with_line('test_search_one.txt')
+        self.indexator.get_index_with_line('test_search_two.txt')
+        self.indexator.get_index_with_line('test_search_three.txt')
+        del self.indexator
+        self.search = SearchEngine('database')
+        result = self.search.get_dict('Find my laptop Alina')
+        os.remove('test_search_one.txt')
+        os.remove('test_search_two.txt')
+        os.remove('test_search_three.txt')
+
 if __name__ == '__main__':
     unittest.main()      
