581232b30be9323c97e47292ab81d1479be7a1d0
AlinaZakharova1997 <43806590+AlinaZakharova1997@users.noreply.github.com>
2018-12-23 22:40:37 +0300

    Update search.py

--- search.py
+++ search.py
@@ -6,6 +6,8 @@ import os
 import shelve
 import indexer
 from indexer import Indexer, Position_Plus
+import tokenizator
+from tokenizator import Tokenizator
 
 
 class SearchEngine(object):
@@ -17,6 +19,7 @@ class SearchEngine(object):
     def __init__(self,database):
 
         self.database = shelve.open(database,writeback=True)
+        self.tokenizator = Tokenizator()
 
     def __del__(self):
         self.database.close()    
@@ -35,5 +38,47 @@ class SearchEngine(object):
         if tok_str in self.database:
             return self.database[tok_str]
         else:
-            return {} 
+            return {}
+
+    def get_dict_many_tokens(self,tok_str):
+        """
+        This function performs searching for positions of given tokens
+        @param tok_str: str containing tokens
+        @return: dictionary, where a key is a filename
+        and a value is a list of positions of all tokens     
+        """
+
+        if not isinstance(tok_str, str):
+            raise TypeError('Input has an unappropriate type!')
+       
+        big_dict_files = []
+        values = []
+        
+        for token in self.tokenizator.token_gen(tok_str):
+            big_dict_files.append(self.get_dict(token.s))
+            values.append((self.get_dict(token.s)).values())
+        print(values)    
+        files = set(big_dict_files[0])    
+        for file_dict in big_dict_files[1:]:
+            files = files.intersection(set(file_dict)) #пересечение файлов
             
+        allpos = [values[0]]
+        for filepos in values[1:]:
+            allpos = allpos + values[1:]
+            
+        allpos_new = []
+        for pos in allpos:
+            if pos not in allpos_new:
+                allpos_new.append(pos)              
+            
+        output_dict = {}    
+        for filename in files:
+            for pos in set(allpos_new):
+                output_dict.setdefault(filename,[]).extend(pos)
+        return output_dict
+        
+        
+            
+
+            
+          
