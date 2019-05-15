6e8a0aa08b57c08c38ee3749bd7437e199eb89ab
AlinaZakharova1997 <43806590+AlinaZakharova1997@users.noreply.github.com>
2019-05-15 09:40:15 +0300

    Update test_windows.py

--- test_windows.py
+++ test_windows.py
@@ -190,6 +190,7 @@ class TestMyCode(unittest.TestCase):
         with self.assertRaises(TypeError):
             self.window.unite_all(12, 'window)))')
 
+
     def test_unite_all(self):
         self.indexator = Indexer('database')
         test_file = open('test_unite_all.txt', 'w') 
@@ -204,11 +205,29 @@ class TestMyCode(unittest.TestCase):
                                                   Position_Plus(0, 21, 28)]}
         self.assertEqual(dictionary, input_dictionary)
         dict_to_function = self.window.unite_all(dictionary, 1)
-        output_dict = {'test_unite_all.txt':[Context_Window('Alina Zakharova',[Position_Plus(0, 0, 5), Position_Plus(0, 6, 15)], 0, 15),
-                                             Context_Window('is a student',[Position_Plus(0, 16, 18), Position_Plus(0, 19, 20),Position_Plus(0, 21, 28)], 16, 28)]}
+        output_dict = {'test_unite_all.txt':[Context_Window('Alina Zakharova is a student',[Position_Plus(0, 0, 5), Position_Plus(0, 6, 15),
+                                                                                            Position_Plus(0, 16, 18), Position_Plus(0, 19, 20),
+                                                                                            Position_Plus(0, 21, 28)], 0, 28)]}
+                                            
         self.assertEqual(dict_to_function,output_dict)
         os.remove('test_unite_all.txt')
-       
+
+   def test_unite(self):
+        self.indexator = Indexer('database')
+        test_file = open('test_unite.txt', 'w') 
+        test_file.write('Alina Zakharova is a student')
+        test_file.close()
+        self.indexator.get_index_with_line('test_unite.txt')
+        del self.indexator
+        self.search = SearchEngine('database')
+        dictionary = {'test_unite.txt':[Position_Plus(0, 0, 5),Position_Plus(0, 6, 15),
+                                            Position_Plus(0, 21, 28)]}
+        dict_to_function = self.window.unite_all(dictionary, 1)
+        output_dict = {'test_unite.txt':[Context_Window('Alina Zakharova',[Position_Plus(0, 0, 5), Position_Plus(0, 6, 15), 0, 15)],
+                                         Context_Window('a student',[Position_Plus(0, 19, 20),Position_Plus(0, 21, 28)], 16, 28)]}             
+        self.assertEqual(dict_to_function,output_dict)
+        os.remove('test_unite.txt')
+
         
 
 if __name__ == '__main__':
