572217de750d0bb42fcd966c3292c51bd903ab40
AlinaZakharova1997 <43806590+AlinaZakharova1997@users.noreply.github.com>
2019-07-12 00:46:12 +0300

    Update get_all_mod.py

--- get_all_mod.py
+++ get_all_mod.py
@@ -1,24 +1,26 @@
-import codecs                         
+import codecs
 from collections import namedtuple
 from urllib.request import urlopen
 import csv
 from collections import OrderedDict
 from lxml import html
 import time
+import os
 
 if __name__ == '__main__':
-    # search link for a word
-    word_search_link = 'http://search1.ruscorpora.ru/syntax-explain.xml?env=alpha&mycorp=&mysent=&mysize=&mysentsize=&dpp=&spp=&spd=&&mode=syntax&notag=1&simple=1&lang=ru&parent1=0&level1=0&lex1=&gramm1=V&flags1=&parent2=1&level2=1&min2=&max2=&link2=on&type2=&lex2=&gramm2=S&flags2=&parent3=1&level3=1&min3=&max3=&link3=on&type3=&lex3=&gramm3=PR&flags3=&text=word-info&requestid=1553071332398&language=ru&source='
-    # tuple that contains information about word and the word itself
-    Word = namedtuple("Word", "word lemma part_of_speech grammar_structure")
+    
     Constructions = open('Constructions.txt', 'w')
-    Constructions.close()
-    global_dict = {}
-    PAUSE_AFTER_FAILURE = 3
-    MAX_RETRY = 3
+    Constructions.close()           
+# search link for a word
+word_search_link = 'http://search1.ruscorpora.ru/syntax-explain.xml?env=alpha&mycorp=&mysent=&mysize=&mysentsize=&dpp=&spp=&spd=&&mode=syntax&notag=1&simple=1&lang=ru&parent1=0&level1=0&lex1=&gramm1=V&flags1=&parent2=1&level2=1&min2=&max2=&link2=on&type2=&lex2=&gramm2=S&flags2=&parent3=1&level3=1&min3=&max3=&link3=on&type3=&lex3=&gramm3=PR&flags3=&text=word-info&requestid=1553071332398&language=ru&source='
+# tuple that contains information about word and the word itself
+Word = namedtuple("Word", "word lemma part_of_speech grammar_structure")
+global_dict = {}
+PAUSE_AFTER_FAILURE = 3
+MAX_RETRY = 3
     
 
-def get_lemma_and_params(suff: str):
+def get_lemma_and_params(suff):
     '''
     This function gets lemma and params with gram info
     @param suff:string containing search suffix to find a word
@@ -30,7 +32,7 @@ def get_lemma_and_params(suff: str):
     params = codecs.decode(info[2].encode('raw-unicode-escape'), 'cp1251')
     return lemma, params
           
-def get_word_info(word: str, suff: str, s_freq_dict, pr_freq_dict, v_freq_dict, adv_freq_dict):
+def get_word_info(word, suff, s_freq_dict, pr_freq_dict, v_freq_dict, adv_freq_dict):
     '''
     This function gets information about a given word and makes frequency dictionaries
     for each part of speech (noun, preposition, verb)
@@ -38,46 +40,42 @@ def get_word_info(word: str, suff: str, s_freq_dict, pr_freq_dict, v_freq_dict, 
     @param suff: string containing search suffix to find a word
     @return: csv files with frequency dictionaries 
     '''
-    print('getwordinfo')
     constr_str = ''
-    if suff not in global_dict:
-        lemma, params = get_lemma_and_params(suff)
-        global_dict[suff] = (lemma, params)
-    else:
-         lemma, params = global_dict[suff]
+    try:
+        if suff not in global_dict:
+            lemma, params = get_lemma_and_params(suff)
+            global_dict[suff] = (lemma, params)
+        else:
+            lemma, params = global_dict[suff]
+        pr_set = set(params.split(",\xa0") )
+        if  's' in pr_set :
+            s_freq_dict.setdefault(lemma, 0)
+            s_freq_dict[lemma] += 1
+            print(lemma,'lemma')
         
-    pr_set = set(params.split(',\xa0') )
-    if  's' in pr_set :
-        s_freq_dict.setdefault(lemma, 0)
-        s_freq_dict[lemma] += 1
-        print('I got a lemma!')
-        print(lemma)
-        
-    elif  'pr' in pr_set:
-        pr_freq_dict.setdefault(lemma, 0)
-        pr_freq_dict[lemma] += 1
-        '''prep_lemmas.write(lemma + '\n')'''
-        print('I got a lemma!')
-        print(lemma)
+        elif  'pr' in pr_set:
+            pr_freq_dict.setdefault(lemma, 0)
+            pr_freq_dict[lemma] += 1
+            print(lemma,'lemma')
       
-    elif 'v' in pr_set:
-        v_freq_dict.setdefault(lemma, 0)
-        v_freq_dict[lemma] += 1
+        elif 'v' in pr_set:
+            v_freq_dict.setdefault(lemma, 0)
+            v_freq_dict[lemma] += 1
+            print(lemma,'lemma')
        
-        print('I got a lemma!')
-        print(lemma)
-       
-    elif 'adv'in pr_set:
-        adv_freq_dict.setdefault(lemma, 0)
-        adv_freq_dict[lemma] += 1
-        print('I got a lemma!')
-        print(lemma)
+        elif 'adv'in pr_set:
+            adv_freq_dict.setdefault(lemma, 0)
+            adv_freq_dict[lemma] += 1
+            print(lemma,'lemma')
         
-    else:
-        print('Error! Tag not found!')
-        print(word)
+        else:
+            print('Error! Tag not found!')
+            print(word)
+        
+    except AssertionError:
+        print('AssertionError')    
    
-def search_highlighted(url: str, s_freq_dict, pr_freq_dict, v_freq_dict, adv_freq_dict):
+def search_highlighted(url, s_freq_dict, pr_freq_dict, v_freq_dict, adv_freq_dict):
     '''
     This function searches all highlighted words and makes constructions
     @param url: string adress of a searching query
@@ -85,17 +83,14 @@ def search_highlighted(url: str, s_freq_dict, pr_freq_dict, v_freq_dict, adv_fre
     '''
     print('search')
     parsed_url = html.parse(url)
-    print('I parsed url')
     constr_str = []
     Constructions = open('Constructions.txt', 'a')
-    print('I opened constructions file')
     '''/html/body/div[4]/ol/li[1]/table/tbody/tr/td/ul/li[1]
     document.querySelector('body > div.content > ol > li:nth-child(1) > table > tbody > tr > td > ul > li:nth-child(1) > span:nth-child(8)')
     '''
     for sent in parsed_url.xpath('//div[@class="content"]/ol/li/table/tr/td/ul/li'):
         constr = ''
         full_sent = sent.xpath('normalize-space(.)').split(' [', 1)[0]
-        print('I am in the circle!')
         for highlighted_word in sent.xpath('span[@class="b-wrd-expl g-em"]'):
             word = highlighted_word.xpath('text()')
             suff = highlighted_word.xpath('@explain')
@@ -104,18 +99,15 @@ def search_highlighted(url: str, s_freq_dict, pr_freq_dict, v_freq_dict, adv_fre
             else:
                 get_word_info(word[0], suff[0], s_freq_dict, pr_freq_dict, v_freq_dict, adv_freq_dict)
                 constr+=word[0]+' '
-        print('I got constr_str!')
         print(constr)
         '''Constructions = open('Constructions.txt', 'a')
         print('I opened constructions file')'''
         Constructions.write(constr + '\n')
-        print('I wrote constr str!')
         '''Constructions.close()
         print('I closed Constructions.txt!')'''
-    Constructions.close()
-    print('I closed Constructions.txt!')    
+    Constructions.close()   
     
-def req(main_link: str, pages: int):
+def req(main_link, pages):
     '''
     This function works with the search link and pushes input into the above functions 
     @param main_link: an url string adress
@@ -130,14 +122,13 @@ def req(main_link: str, pages: int):
         print('I got page %s'%i)
         for n_try in range(MAX_RETRY):
             try:
-                print('I try!')
                 all_highlighted = search_highlighted(main_link+'&p=%s' % i, s_freq_dict, pr_freq_dict, v_freq_dict, adv_freq_dict)
                 break
-            except Exception:
-                raise 
+            except OSError:
                 time.sleep(PAUSE_AFTER_FAILURE)
-                print('Smth BAD happened!')
-
+                print('BAD OSError happened!')
+              
+                
     pr_freq_dict_sorted = OrderedDict(sorted(pr_freq_dict.items(), key = lambda t: t[1], reverse=True))
     with open('Prep_dict.csv', 'w') as csv_file:
         writer = csv.writer(csv_file, delimiter= ';')
@@ -158,18 +149,9 @@ def req(main_link: str, pages: int):
         writer = csv.writer(csv_file, delimiter= ';')
         for key, value in adv_freq_dict_sorted.items():
             writer.writerow([key,value])         
-                   
-# блоки try, exept отлавливать только ту ошибку, которая возникает, когда сервер отвалился! а не все возможные ошибки!!!!
-# отдельный скрипт, который проверит, какие леммы вошли в макушку частотника и то, что вошло мы и обработаем в онтологии и работать будем с леммами
-# какой процент составляют все конструкции со стрелочной омонимии
-# this is the main link
-req(
-'http://search1.ruscorpora.ru/syntax.xml?out=normal&kwsz=4&dpp=100&spd=100&spp=500&seed=10330&env=alpha&mycorp=&mysent=&mysize=&mysentsize=&text=lexgramm&mode=syntax&notag=1&simple=1&lang=ru&parent1=0&level1=0&lex1=&gramm1=V&flags1=&parent2=1&level2=1&min2=&max2=&link2=on&type2=&lex2=&gramm2=S&flags2=&parent3=2&level3=2&min3=1&max3=&link3=on&type3=&lex3=&gramm3=PR&flags3=&parent4=3&level4=3&min4=1&max4=&link4=on&type4=&lex4=&gramm4=S&flags4=',
-7)
-# http://search1.ruscorpora.ru/syntax.xml?env=alpha&mycorp=&mysent=&mysize=&mysentsize=&dpp=&spp=&spd=&text=lexgramm&mode=syntax&notag=1&simple=1&lang=ru&parent1=0&level1=0&lex1=&gramm1=V&flags1=&parent2=1&level2=1&min2=&max2=&link2=on&type2=&lex2=&gramm2=S&flags2=&parent3=1&level3=1&min3=1&max3=&link3=on&type3=&lex3=&gramm3=PR&flags3=&parent4=3&level4=2&min4=1&max4=&link4=on&type4=&lex4=&gramm4=S&flags4=
-'http://processing.ruscorpora.ru/syntax.xml?env=alpha&mycorp=&mysent=&mysize=&mysentsize=&dpp=&spp=&spd=&text=lexgramm&mode=syntax&notag=1&simple=1&lang=ru&parent1=0&level1=0&lex1=&gramm1=V&flags1=&parent2=1&level2=1&min2=1&max2=&link2=on&type2=&lex2=&gramm2=S&flags2=&parent3=2&level3=2&min3=1&max3=&link3=on&type3=&lex3=&gramm3=PR&flags3=&parent4=3&level4=3&min4=1&max4=&link4=on&type4=&lex4=&gramm4=S&flags4= '
-
-
-
+    
+    
+req('http://search1.ruscorpora.ru/syntax.xml?out=normal&kwsz=4&dpp=50&spd=100&spp=500&seed=10330&env=alpha&mycorp=&mysent=&mysize=&mysentsize=&text=lexgramm&mode=syntax&notag=1&simple=1&lang=ru&parent1=0&level1=0&lex1=&gramm1=V&flags1=&parent2=1&level2=1&min2=&max2=&link2=on&type2=&lex2=&gramm2=S&flags2=&parent3=2&level3=2&min3=1&max3=&link3=on&type3=&lex3=&gramm3=PR&flags3=&parent4=3&level4=3&min4=1&max4=&link4=on&type4=&lex4=&gramm4=S&flags4=',
+    13)                    
 
 
