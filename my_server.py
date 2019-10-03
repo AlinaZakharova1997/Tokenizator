5bba95f05afe4ade585da0f2cedda09637ebe722
AlinaZakharova1997 <43806590+AlinaZakharova1997@users.noreply.github.com>
2019-10-03 20:22:18 +0300

    Update my_server.py

--- my_server.py
+++ my_server.py
@@ -19,6 +19,14 @@ class RequestHandler(BaseHTTPRequestHandler):
                         <form method="post">
                             <input type="text" name="query">
                             <input type="submit" value="Search">
+                            <label for="limit">
+                            Docs per page
+                            <input type="number" name="limit">
+                            </label>
+                            <label for="offset">
+                            Start from doc number
+                            <input type="number" name="offset">
+                            </label>
                         </form>
                     </body>
                 </html>
@@ -26,36 +34,57 @@ class RequestHandler(BaseHTTPRequestHandler):
         self.wfile.write(bytes(html, encoding="utf-8"))
 
     def do_POST(self):
+        '''
+        This function sends search results
+        '''
+       
         form = FieldStorage(fp=self.rfile, headers=self.headers, environ={'REQUEST_METHOD':'POST'})
         query = str(form.getvalue('query'))
+        limit = int(form.getvalue('limit'))
+        if not limit:
+            limit = 15
+        offset = int(form.getvalue('offset'))
+        if not offset:
+            offset = 0
         my_search = SearchEngine('TolstoyDataBase')
+        print(query)
         final = my_search.unite_extended(query, 3)
         # field, button and query
-        self.send_response(200)
+        self.send_response(250)
         self.send_header("Content-type", "text/html; charset=utf-8")
         self.end_headers()
         self.wfile.write(bytes("""
                 <html>
                     <body>
                         <form method="post">
-                            <input type="text" name="query" value = "%s"/>
-                            <input type="submit" value="Search">
-                        </form>
-                 """ % query, encoding="utf-8"))
-        
+                            <input type="text" name="query" value="%s"/>
+                            <input type="submit" value="Search"/>
+                            <label for="limit">
+                            Docs per page
+                            <input type="number" name="limit" value="%d"/>
+                            </label>
+                            <label for="offset">
+                            Start from doc number
+                            <input type="number" name="offset" value="%d"/>
+                            </label>
+                """ % (query,limit,offset), encoding="utf-8"))
         # the beginning of ordered list
         self.wfile.write(bytes('<ol>', encoding="utf-8")) 
         if not final:
-             self.wfile.write('NOT FOUND, SORRY', encoding="utf-8")
-        for filename in final:
-            self.wfile.write(bytes('<li><p> %s </p></li>' % filename, encoding="utf-8"))
-            # the beginning of unordered list
-            self.wfile.write(bytes('<ul>', encoding="utf-8")) 
-            for window in final[filename]:
-                hi_str = window.highlight_window()
-                self.wfile.write(bytes('<li><p> %s </p></li>' % hi_str, encoding="utf-8"))
-            self.wfile.write(bytes('</ul>', encoding="utf-8"))  
-        self.wfile.write(bytes ('</ol></body></html>', encoding="utf-8"))
+            self.wfile.write(bytes('NOT FOUND, SORRY', encoding="utf-8"))
+        for number,filename in enumerate (final):
+            if number == offset and number < limit:
+                self.wfile.write(bytes('<li><p> %s </p></li>' % filename, encoding ="utf-8"))
+                # the beginning of unordered list
+                self.wfile.write(bytes('<ul>', encoding="utf-8"))
+                for window in final[filename]:
+                    hi_str = window.highlight_window()
+                    self.wfile.write(bytes('<li><p> %s </p></li>' % hi_str, encoding="utf-8"))
+                self.wfile.write(bytes('</ul>', encoding="utf-8"))
+            if number == limit:
+                break
+        self.wfile.write(bytes("""</ol></form></body></html>""", encoding="utf-8"))
+
 
 def main():
     my_server = HTTPServer(('', 80), RequestHandler)
@@ -63,4 +92,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
