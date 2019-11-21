315ee30262b1332528a8b01399cb0ee138972780
AlinaZakharova1997 <43806590+AlinaZakharova1997@users.noreply.github.com>
2019-11-21 11:08:32 +0300

    Update test_selection.py

--- test_selection.py
+++ test_selection.py
@@ -5,9 +5,9 @@ class TestMyCode(unittest.TestCase):
    
     def test_one(self):
         
-        lists = [[('собака', 23,'noun'),('кот', 15,'noun'), ('девочка', 30,'noun')],
-                 [('бегать', 20,'verb'),('быть', 45,'verb'), ('дискутировать', 3,'verb'), ('булдыжничать', 1,'verb')],
-                 [('с', 10,'prep'),('в', 56,'prep')]]
+        lists = [[('девочка', 30,'noun'),('собака', 23,'noun'),('кот', 15,'noun')],
+                 [('быть', 45,'verb'),('бегать', 20,'verb'),('дискутировать', 3,'verb'), ('булдыжничать', 1,'verb')],
+                 [('в', 56,'prep'),('с', 10,'prep')]]
         
         result = list(make_selection.lemma_generator(lists))
         cool = [('в','prep'),('быть','verb'),('девочка','noun'),('собака','noun'),('бегать','verb'),('кот','noun'),('с','prep'),('дискутировать','verb'),('булдыжничать','verb')]
