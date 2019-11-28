f86bfb3dc544e0075cbcb69bfbc9f7e422ee312d
AlinaZakharova1997 <43806590+AlinaZakharova1997@users.noreply.github.com>
2019-11-28 14:06:38 +0300

    Update test_search.py

--- test_search.py
+++ test_search.py
@@ -203,6 +203,7 @@ class TestMyCode(unittest.TestCase):
         self.assertEqual(dict_to_function, output_dict)
         os.remove('test_unite.txt')
         
+   
     def test_TypeError_unite_extended(self):
         del self.indexator
         self.search = SearchEngine('database')
@@ -237,7 +238,26 @@ class TestMyCode(unittest.TestCase):
         fine_result = {'test_unite_extended.txt':[Context_Window('Alina Zakharova is a student!! Smart Student Alina Zakharova tries to write programs.',[Position_Plus(0, 0, 5), Position_Plus(0, 6, 15)], 0, 30),
                                                   Context_Window('Alina Zakharova is a student!! Smart Student Alina Zakharova tries to write programs.',[Position_Plus(0,45,50),Position_Plus(0,51,60)], 31, 85)]}
         self.assertEqual(result, fine_result)
-        os.remove('test_unite_extended.txt')    
+        os.remove('test_unite_extended.txt')
+        
+    def test_unite_extended_two_files(self):
+        test_file_one = open('test_unite_extended.txt', 'w') 
+        test_file_one.write('Smart Student Alina Zakharova tries to write programs.')
+        test_file_one.close()
+        test_file_two = open('test_unite_extended_two.txt', 'w') 
+        test_file_two.write('Alina Zakharova is a student!!')
+        test_file_two.close()
+        self.indexator.get_index_with_line('test_unite_extended.txt')
+        self.indexator.get_index_with_line('test_unite_extended_two.txt')
+        del self.indexator      
+        self.search = SearchEngine('database')
+        query = 'Zakharova'
+        result = self.search.unite_extended(query, 1)
+        fine_result = {'test_unite_extended.txt':[Context_Window('Smart Student Alina Zakharova tries to write programs.',[Position_Plus(0,20,29)], 0, 54)],
+                       'test_unite_extended_two.txt':[Context_Window('Alina Zakharova is a student!!',[Position_Plus(0,6,15)], 0, 30)]}
+        self.assertEqual(result, fine_result)
+        os.remove('test_unite_extended.txt')
+        os.remove('test_unite_extended_two.txt')  
   
     def test_query_search(self):
        test_file_one = open('test_query_search.txt', 'w') 
@@ -258,7 +278,7 @@ class TestMyCode(unittest.TestCase):
        test_file_one.write('Alina Zakharova is a student!!')
        test_file_one.close()
        test_file_two = open('test_query_search_two.txt', 'w') 
-       test_file_two.write('Little Zakharova loves big apples')
+       test_file_two.write('Little Zakharova loves big apples.')
        test_file_two.close()
        self.indexator.get_index_with_line('test_query_search_one.txt')
        self.indexator.get_index_with_line('test_query_search_two.txt')
@@ -267,7 +287,7 @@ class TestMyCode(unittest.TestCase):
        query = 'Zakharova'
        result = self.search.query_search(query, 1)
        fine_result = {'test_query_search_one.txt':['Alina <b>Zakharova</b> is a student!!'],
-                      'test_query_search_two.txt':['Little <b>Zakharova</b> loves big apples']}
+                      'test_query_search_two.txt':['Little <b>Zakharova</b> loves big apples.']}
        self.assertEqual(result, fine_result)
        os.remove('test_query_search_one.txt')
        os.remove('test_query_search_two.txt')   
@@ -346,7 +366,7 @@ class TestMyCode(unittest.TestCase):
         test_file_one.write('Smart Student Alina Zakharova is a linguist!! Smart Student Alina Zakharova tries to write programs. Python is easy, Zakharova, keep calm caaalm!!!')
         test_file_one.close()
         test_file_two = open('test_qulim_search_2.txt', 'w') 
-        test_file_two.write('Little funny girl Alina Zakharova loves big apples. Also extra smart student Alina Zakharova loves rock music.')
+        test_file_two.write('Little funny girl Alina loves big apples. Also extra smart student Alina Zakharova loves rock music.')
         test_file_two.close()
         test_file_three = open('test_qulim_search_3.txt', 'w') 
         test_file_three.write('Beautiful and perfect Alina Zakharova loves sweets and black coffee. Also cute and nice Alina Zakharova loves Disney cartoons.')
@@ -360,20 +380,20 @@ class TestMyCode(unittest.TestCase):
         query = 'Alina Zakharova'
         result = self.search.qulim_search(query, win_size=1, limit=2, offset=1, doc_limof =[(2,0), (1,0)] )
         fine_result = {
-                      'test_qulim_search_2.txt': ['Little funny girl <b>Alina</b> <b>Zakharova</b> loves big apples.',
+                      'test_qulim_search_2.txt': ['Little funny girl <b>Alina</b> loves big apples.',
                                                     'Also extra smart student <b>Alina</b> <b>Zakharova</b> loves rock music.'],
                       'test_qulim_search_3.txt':['Beautiful and perfect <b>Alina</b> <b>Zakharova</b> loves sweets and black coffee.']}
         self.assertEqual(result, fine_result)
         os.remove('test_qulim_search_1.txt')
         os.remove('test_qulim_search_2.txt')
-        os.remove('test_qulim_search_3.txt')  
-
-     def test_qulim_search_four(self):
+        os.remove('test_qulim_search_3.txt')
+        
+    def test_qulim_search_four(self):
         test_file_one = open('test_qulim_search_1.txt', 'w') 
         test_file_one.write('Умная ученица Алина Захарова - лингвист !! Умная ученица Алина Захарова пытается писать программы. Питон - это просто, Захарова, сохраняй спокойствие!')
         test_file_one.close()
         test_file_two = open('test_qulim_search_2.txt', 'w') 
-        test_file_two.write('Маленькая смешная девочка Алина Захарова любит большие яблоки. Также очень умная ученица Алина Захарова любит рок-музыку.')
+        test_file_two.write('Маленькая смешная девочка Алина любит большие яблоки. Также очень умная ученица Алина Захарова любит рок-музыку.')
         test_file_two.close()
         test_file_three = open('test_qulim_search_3.txt', 'w') 
         test_file_three.write('Прекрасная и идеальная Алина Захарова любит сладости и черный кофе. Также милая и милая Алина Захарова любит мультфильмы Диснея.')
@@ -387,13 +407,56 @@ class TestMyCode(unittest.TestCase):
         query = 'Алина Захарова'
         result = self.search.qulim_search(query, win_size=1, limit=2, offset=1, doc_limof =[(2,0), (1,0)] )
         fine_result = {
-                      'test_qulim_search_2.txt': ['Маленькая смешная девочка <b>Алина</b> <b>Захарова</b> любит большие яблоки.',
+                      'test_qulim_search_2.txt': ['Маленькая смешная девочка <b>Алина</b> любит большие яблоки.',
                                                     'Также очень умная ученица <b>Алина</b> <b>Захарова</b> любит рок-музыку.'],
                       'test_qulim_search_3.txt':['Прекрасная и идеальная <b>Алина</b> <b>Захарова</b> любит сладости и черный кофе.']}
         self.assertEqual(result, fine_result)
         os.remove('test_qulim_search_1.txt')
         os.remove('test_qulim_search_2.txt')
-        os.remove('test_qulim_search_3.txt')  
+        os.remove('test_qulim_search_3.txt')
+        
+    def test_qulim_search_five(self):
+     
+        test_file_two = open('test_qulim_search_1.txt', 'w') 
+        test_file_two.write('Узнав, что Безухов в Орле, Вилларский, хотя и никогда не был коротко знаком с ним, приехал к нему с теми заявлениями дружбы и близости, которые выражают обыкновенно друг другу люди, встречаясь в пустыне. Вилларский скучал в Орле и был счастлив, встретив человека одного с собой круга и с одинаковыми, как он полагал, интересами. Умный и смелый Пьер хочет быть декабристом.')
+        test_file_two.close()
+        test_file_three = open('test_qulim_search_2.txt', 'w') 
+        test_file_three.write('Кому, в особенности ему, какое дело было до того, что узнают или не узнают, что имя их пленного было граф Безухов? Узнав, что Безухов в Орле, Вилларский, хотя и никогда не был коротко знаком с ним, приехал к нему с теми заявлениями дружбы и близости, которые выражают обыкновенно друг другу люди, встречаясь в пустыне. В числе отбитых Денисовым и Долоховым русских пленных был Пьер Безухов.' )
+        test_file_three.close()
+        self.indexator.get_index_with_line('test_qulim_search_1.txt')
+        self.indexator.get_index_with_line('test_qulim_search_2.txt')
+        del self.indexator
+        self.search = SearchEngine('database')
+        query = 'Пьер Безухов'
+        result = self.search.qulim_search(query, win_size=1, limit=2, offset=0, doc_limof =[(1,0), (2,0)] )
+        fine_result = {'test_qulim_search_1.txt': ['Узнав, что <b>Безухов</b> в Орле, Вилларский, хотя и никогда не был коротко знаком с ним, приехал к нему с теми заявлениями дружбы и близости, которые выражают обыкновенно друг другу люди, встречаясь в пустыне.'],
+                      'test_qulim_search_2.txt':['Кому, в особенности ему, какое дело было до того, что узнают или не узнают, что имя их пленного было граф <b>Безухов</b>? Узнав, что <b>Безухов</b> в Орле, Вилларский, хотя и никогда не был коротко знаком с ним, приехал к нему с теми заявлениями дружбы и близости, которые выражают обыкновенно друг другу люди, встречаясь в пустыне.',
+                                                 'В числе отбитых Денисовым и Долоховым русских пленных был <b>Пьер</b> <b>Безухов</b>.']}
+        self.assertEqual(result, fine_result)
+        os.remove('test_qulim_search_1.txt')
+        os.remove('test_qulim_search_2.txt')
+        
+    def test_qulim_search_six(self):
+     
+        test_file_two = open('test_qulim_search_1.txt', 'w') 
+        test_file_two.write('Узнав, что Безухов в Орле, Вилларский, хотя и никогда не был коротко знаком с ним, приехал к нему с теми заявлениями дружбы и близости, которые выражают обыкновенно друг другу люди, встречаясь в пустыне. Вилларский скучал в Орле и был счастлив, встретив человека одного с собой круга и с одинаковыми, как он полагал, интересами. Умный и смелый Пьер хочет быть декабристом.')
+        test_file_two.close()
+        test_file_three = open('test_qulim_search_2.txt', 'w') 
+        test_file_three.write('Кому, в особенности ему, какое дело было до того, что узнают или не узнают, что имя их пленного было граф Безухов? Узнав, что Безухов в Орле, Вилларский, хотя и никогда не был коротко знаком с ним, приехал к нему с теми заявлениями дружбы и близости, которые выражают обыкновенно друг другу люди, встречаясь в пустыне. В числе отбитых Денисовым и Долоховым русских пленных был Пьер Безухов.' )
+        test_file_three.close()
+        self.indexator.get_index_with_line('test_qulim_search_1.txt')
+        self.indexator.get_index_with_line('test_qulim_search_2.txt')
+        del self.indexator
+        self.search = SearchEngine('database')
+        query = 'Пьер Безухов Пьер'
+        result = self.search.qulim_search(query, win_size=1, limit=2, offset=0, doc_limof =[(1,0), (2,0)] )
+        fine_result = {'test_qulim_search_1.txt': ['Узнав, что <b>Безухов</b> в Орле, Вилларский, хотя и никогда не был коротко знаком с ним, приехал к нему с теми заявлениями дружбы и близости, которые выражают обыкновенно друг другу люди, встречаясь в пустыне.'],
+                      'test_qulim_search_2.txt':['Кому, в особенности ему, какое дело было до того, что узнают или не узнают, что имя их пленного было граф <b>Безухов</b>? Узнав, что <b>Безухов</b> в Орле, Вилларский, хотя и никогда не был коротко знаком с ним, приехал к нему с теми заявлениями дружбы и близости, которые выражают обыкновенно друг другу люди, встречаясь в пустыне.',
+                                                 'В числе отбитых Денисовым и Долоховым русских пленных был <b>Пьер</b> <b>Безухов</b>.']}
+        self.assertEqual(result, fine_result)
+        os.remove('test_qulim_search_1.txt')
+        os.remove('test_qulim_search_2.txt')
+
 
     def test_qulim_search_empty(self):
         test_file_one = open('test_qulim_search_1.txt', 'w') 
@@ -418,3 +481,4 @@ class TestMyCode(unittest.TestCase):
        
 if __name__ == '__main__':
     unittest.main()        
+
